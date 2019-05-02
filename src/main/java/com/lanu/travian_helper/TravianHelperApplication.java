4afc8e71c8db555eaf5451715634cbb110b0d230
Serhiy K <mrlanu@gmail.com>
2019-05-01 21:18:25 -0500

    init commit

---
+++ src/main/java/com/lanu/travian_helper/TravianHelperApplication.java
@@ -0,0 +1,13 @@
+package com.lanu.travian_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravianHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravianHelperApplication.class, args);
+    }
+
+}

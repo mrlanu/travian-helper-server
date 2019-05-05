0a20ccfecf271dab947dfe92ca99059fef6ce822
Serhiy K <mrlanu@gmail.com>
2019-05-05 00:42:19 -0500

    HtmlUnit added

--- src/main/java/com/lanu/travian_helper/controllers/MainController.java
+++ src/main/java/com/lanu/travian_helper/controllers/MainController.java
@@ -1,5 +1,7 @@
 package com.lanu.travian_helper.controllers;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
 import com.lanu.travian_helper.models.Attack;
 import com.lanu.travian_helper.models.AttacksString;
 import com.lanu.travian_helper.services.ParsingService;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -25,6 +28,30 @@ public class MainController {
 
     @PostMapping("/attacks")
     public List<Attack> getAllAttacks(@Valid @RequestBody AttacksString stringAttacksForParse){
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage("https://ts4.travian.ru/dorf1.php");
+            System.out.println(page.getTitleText());
+
+            final HtmlForm form = page.getFormByName("login");
+            final HtmlButton button = form.getButtonByName("s1");
+            final HtmlTextInput textField = form.getInputByName("name");
+            final HtmlPasswordInput textFieldPass = form.getInputByName("password");
+            textField.type("Баба Яга");
+            textFieldPass.type("28333555");
+            final HtmlPage page2 = button.click();
+
+            final HtmlPage page3 = page2.getAnchorByHref("?newdid=18091&").click();
+
+            System.out.println("KURWA++++++++>>>>>>>>>>>>>>>");
+            System.out.println(page2.getAnchorByHref("?newdid=18091&"));
+            System.out.println("Fuck");
+            List<HtmlAnchor> list = page3.getAnchors();
+            for (HtmlAnchor a : list)System.out.println(a + "\n");
+
+
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
         return parsingService.getAllAttacks(stringAttacksForParse);
     }
 }
